b5492e8262d11ec5390ea51a36a7f9680f1fc78c
Connor Monahan <cma2714@gmail.com>
2020-11-01 01:02:03 -0600

    Zero hour game!

---
+++ Assets/gamecontorller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gamecontorller : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.Range(0, 20) == 4)
+        {
+            int which = Random.Range(0, 3);
+            if (GameObject.Find("enemy" + (1 + which) + "(Clone)"))
+            {
+
+            }
+            else
+            {
+                Instantiate(prefabs[which]);
+            }
+        }
+
+        if (GameObject.Find("enemy1(Clone)") && GameObject.Find("enemy2(Clone)") && GameObject.Find("enemy3(Clone)"))
+        {
+            SceneManager.LoadScene("Lose");
+        }
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    public Text scoreUI;
+    public Transform attackZone;
+
+    private int bonks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        bonks = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(Vector2.left * 1000f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(Vector2.right * 1000f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Collider2D[] hits = Physics2D.OverlapCircleAll(attackZone.position, 0.7f);
+            foreach (Collider2D hit in hits)
+            {
+                if (hit.GetComponent<fetus>())
+                {
+                    Destroy(hit.gameObject);
+                    Bonk();
+                }
+            }
+        }
+    }
+
+
+    void Bonk()
+    {
+        bonks++;
+        scoreUI.text = "Convicts bonked: " + bonks;
+    }
+}
